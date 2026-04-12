d8c3817db37c8af256a23365ed4c22291798d666
sunjangyo12 <sunjangyo12@gmail.com>
2026-04-13 06:30:04 +0700

    update section addr

--- src/main.rs
+++ src/main.rs
@@ -271,17 +271,17 @@ fn main() {
             permissions: Perm(PERM_READ),
         },
         Section {
-            file_off:    0x0000000000000000,
-            virt_addr:   VirtAddr(0x0000000000010000),
-            file_size:   0x0000000000000190,
-            mem_size:    0x0000000000000190,
+            file_off:    0x0000000000000190,            // second LOAD
+            virt_addr:   VirtAddr(0x0000000000011190),
+            file_size:   0x0000000000002598,
+            mem_size:    0x0000000000002598,
             permissions: Perm(PERM_EXEC),
         },
         Section {
-            file_off:    0x0000000000000000,
-            virt_addr:   VirtAddr(0x0000000000010000),
-            file_size:   0x0000000000000190,
-            mem_size:    0x0000000000000190,
+            file_off:    0x0000000000002728,            // third LOAD
+            virt_addr:   VirtAddr(0x0000000000014728),
+            file_size:   0x00000000000000f8,
+            mem_size:    0x0000000000000750,
             permissions: Perm(PERM_READ | PERM_WRITE),
         },
     ]).expect("Failed to load test application into address space");
