d1fa8e114b6cade410070cf45a86ea36aaf49b68
sunjangyo12 <sunjangyo12@gmail.com>
2026-04-12 11:35:29 +0700

    add memory method

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,18 @@
+const PERM_READ:  u8 = 1 << 0;
+const PERM_WRITE: u8 = 1 << 1;
+const PERM_EXEC:  u8 = 1 << 2;
+const PERM_RAW:   u8 = 1 << 3;
+
+/// A permissions byte which corresponds to a memory byte and defines
+/// the permissions it has
 #[repr(transparent)]
 #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
 struct Perm(u8);
+
+/// A guest virtual address
+#[repr(transparent)]
+#[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord)]
+struct VirtAddr(usize);
 
 /// An isolated memory space
 struct Mmu {
@@ -10,6 +22,9 @@ struct Mmu {
 
     // Holds the permission bytes for the corresponding byte in memory
     permissions: Vec<Perm>,
+
+    /// Current base address of the next allocation
+    cur_alc: VirtAddr,
 }
 
 impl Mmu {
@@ -18,7 +33,38 @@ impl Mmu {
         Mmu {
             memory:      vec![0; size],
             permissions: vec![Perm(0); size],
+            cur_alc:     VirtAddr(0x10000),
         }
+    }
+    /// Allocate a region of memory as RW in the address space
+    pub fn allocate(&mut self, size: usize) -> Option<VirtAddr> {
+        // 16-byte align the allocation
+        let align_size = (size + 0xf) & !0xf;
+
+        // Get the current allocation base
+        let base = self.cur_alc;
+
+        // Cannot allocate
+        if base.0 >= self.memory.len() {
+            return None
+        }
+
+        // Update the allocation size
+        self.cur_alc = VirtAddr(self.cur_alc.0.checked_add(align_size)?);
+
+        // Could not satisfy allocation without going OOM
+        if self.cur_alc.0 > self.memory.len() {
+            return None;
+        }
+
+        // Mark the memory as un-initialized and writable
+        self.set_permissions(base, size, Perm(PERM_RAW | PERM_WRITE));
+
+        Some(base)
+    }
+
+    /// Apply permissions to a region of memory
+    pub fn set_permissions(&mut self, addr: VirtAddr, size: usize, perm: Perm) {
     }
 }
 
