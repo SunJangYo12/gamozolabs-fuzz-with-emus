acd1049f49c89118c69aaa4cb365199ec98e4a3f
sunjangyo12 <sunjangyo12@gmail.com>
2026-04-17 16:28:31 +0700

    Add target objdump

--- zz.sh
+++ zz.sh
@@ -1,6 +1,11 @@
 
 while [ 1 ]; do
     read -p "commit: " msg
+
+    git add src/emulator.rs
+    git add src/mmu.rs
+    git add src/primitive.rs
     git add src/main.rs
+
     git commit -m "$msg"
 done
