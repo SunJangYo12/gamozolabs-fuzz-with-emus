63858c145cebe0aac5dc78b9131448e3256cfa2b
sunjangyo12 <sunjangyo12@gmail.com>
2026-04-24 06:08:23 +0700

    Commit before JIT

--- src/emulator.rs
+++ src/emulator.rs
@@ -1,8 +1,45 @@
 //! A 64-bit RISC-V RV64i interpreter
 
 use std::fmt;
-use std::sync::Arc;
 use crate::mmu::{VirtAddr, Perm, Mmu, PERM_EXEC};
+
+#[cfg(target_os="windows")]
+pub fn alloc_rwx(size: usize) -> &'static mut [u8] {
+    extern {
+        fn VirtualAlloc(lpAddress: *const u8, dwSize: usize,
+                        flAllocationType: u32, flProtect: u32) -> *mut u8;
+    }
+
+    unsafe {
+        const PAGE_EXECUTE_READWRITE: u32 = 0x40;
+
+        const MEM_COMMIT:  u32 = 0x00001000;
+        const MEM_RESERVE: u32 = 0x00002000;
+
+        let ret = VirtualAlloc(0 as *const _, size, MEM_COMMIT | MEM_RESERVE,
+                               PAGE_EXECUTE_READWRITE);
+        assert!(!ret.is_null());
+
+        std::slice::from_raw_parts_mut(ret, size)
+    }
+}
+
+#[cfg(target_os="linux")]
+pub fn alloc_rwx(size: usize) -> &'static mut [u8] {
+    extern {
+        fn mmap(addr: *mut u8, length: usize, prot: i32, flags: i32, fd: i32,
+                offset: usize) -> *mut u8;
+    }
+
+    unsafe {
+        // Alloc RWX and MAP_PRIVATE | MAP_ANON
+        let ret = mmap(0 as *mut u8, size, 7, 34, -1, 0);
+        assert!(!ret.is_null());
+        
+        std::slice::from_raw_parts_mut(ret, size)
+    }
+}
+
 
 /// 64-bit RISC-V registers
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
