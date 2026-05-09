a2e6b432f6426b4ea5a5ce20d27e1bc16b0a0c26
sunjangyo12 <sunjangyo12@gmail.com>
2026-05-10 06:06:17 +0700

    masih, test no return function

--- test-jit/test.rs
+++ test-jit/test.rs
@@ -1,4 +1,5 @@
 #![no_std]
+#![feature(asm)]
 
 pub struct Registers {
     rax: u64,
@@ -7,10 +8,19 @@ pub struct Registers {
     rdx: u64,
 }
 
+#[inline(always)]
+fn noret() -> ! {
+    unsafe { asm!(".byte 0x41, 0x41, 0x41, 0x41", options(noreturn)) }
+}
+
 pub fn main(regs: &mut Registers) {
     regs.rax += 1;
     regs.rax -= 5;
     if regs.rax > 5 {
-        regs.rbx += 5;
+        regs.rax += 5;
+    } else {
+        regs.rax -= 5;
     }
+
+    noret();
 }
