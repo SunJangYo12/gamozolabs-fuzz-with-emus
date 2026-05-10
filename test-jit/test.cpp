67f4782727916bdcbc71c0c868650ae9cd4fbbcb
sunjangyo12 <sunjangyo12@gmail.com>
2026-05-10 18:47:14 +0700

    masih, masih

--- test-jit/test.cpp
+++ test-jit/test.cpp
@@ -7,15 +7,7 @@ struct _registers {
     uint64_t rdx;
 };
 
-
-__attribute__((always_inline))
-_Noreturn
-static void jmpout(void *jmptbl, struct _registers *regs);
-
-_Noreturn
-void inst_0000(void *jmptbl, struct _registers *regs) {
-    volatile char foo[1024];
-
+uint64_t inst_0000(void *jmptbl, struct _registers *regs) {
 inst_0000:
     regs->rax += 1;
     goto inst_0004;
@@ -24,25 +16,13 @@ inst_0004:
     goto inst_0008;
 inst_0008:
     if (regs->rax != 0) {
-        foo[53] = 6;
         goto inst_0010;
     }
     goto inst_000c;
 inst_000c:
-    jmpout(jmptbl, regs);
+    return 0x5483;
     goto inst_0010;
 inst_0010:
     regs->rax = 17;
-    jmpout(jmptbl, regs);
-}
-
-
-__attribute__((always_inline))
-_Noreturn
-static void jmpout(void *jmptbl, struct _registers *regs) {
-    asm volatile(R"goodstr(
-        int3
-    )goodstr");
-
-    __builtin_unreachable();
+    return 0x54833429;
 }
