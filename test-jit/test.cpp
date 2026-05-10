a635ff96d9d512c65efe531c75c4494f79ddd653
sunjangyo12 <sunjangyo12@gmail.com>
2026-05-10 18:34:00 +0700

    masih, masih

--- test-jit/test.cpp
+++ test-jit/test.cpp
@@ -16,14 +16,18 @@ _Noreturn
 void inst_0000(void *jmptbl, struct _registers *regs) {
 inst_0000:
     regs->rax += 1;
+    goto inst_0004;
 inst_0004:
     regs->rax -= 5;
+    goto inst_0008;
 inst_0008:
     if (regs->rax != 0) {
         goto inst_0010;
     }
+    goto inst_000c;
 inst_000c:
     jmpout(jmptbl, regs);
+    goto inst_0010;
 inst_0010:
     regs->rax = 17;
     jmpout(jmptbl, regs);
