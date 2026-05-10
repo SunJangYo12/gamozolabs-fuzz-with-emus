16ce404ee4936fc3e1a2b5fa762ac4e19fe63c9d
sunjangyo12 <sunjangyo12@gmail.com>
2026-05-10 17:57:40 +0700

    masih, masih

--- test-jit/test.c
+++ test-jit/test.c
@@ -5,9 +5,23 @@ struct _registers {
     uint64_t rbx;
     uint64_t rcx;
     uint64_t rdx;
+};
+
+void jmpout() {
 }
 
-inst_0000(struct _registers *regs) {
+void inst_0000(struct _registers *regs) {
 inst_0000:
-    regs->rax += 5;
+    regs->rax += 1;
+inst_0004:
+    regs->rax -= 5;
+inst_0008:
+    if (regs->rax != 0) {
+        goto inst_0010;
+    }
+inst_000c:
+    jmpout();
+inst_0010:
+    regs->rax = 17;
+    jmpout();
 }
