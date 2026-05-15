dfeaba4121fefad0f693e8ed47f738186068d5cd
sunjangyo12 <sunjangyo12@gmail.com>
2026-05-15 08:23:19 +0700

    masih, update dirty WARNING not test or bts instruction hit because error in addr setlocale

--- src/emulator.rs
+++ src/emulator.rs
@@ -2502,12 +2502,14 @@ extern "C" void start(struct _state *state) {
     *({}*)(state->permissions + addr) |= perms >= 3;*/
 
     auto block = addr / {};
-    auto idx   = block / 64;
-    auto bit   = 1 << (block % 64);
-    if ((state->dirty_bitmap[idx] & bit) == 0) {{
-         state->dirty[state->dirty_idx++] = block;
-    }}
-
+    asm(
+        "bts %[block], %[_BitBase]\n\t"
+        "jc 2f\n\t"
+        "2:\n\t"
+        :
+        : [_BitBase] "m" (*state->dirty_bitmap), [block] "r" (block)
+        : "cc" // clobber condition code
+    );
     "#, storetyp, storetyp, perm_mask, perm_mask, pc.0, storetyp,
         raw_mask, storetyp, DIRTY_BLOCK_SIZE);
 
